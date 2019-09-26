a722a0bd25380c5c5e6ecf57e1518c09a9c6ea3d
dnkato <diane.n.kato@gmail.com>
2019-09-25 21:36:56 -0700

    move unit tests from TestGameUI to TestGame

--- Wheel of Azure/UnitTests/TestGame.cs
+++ Wheel of Azure/UnitTests/TestGame.cs
@@ -23,12 +23,7 @@ namespace UnitTests
         public void TestGameStart_PlayerOneWins(string phrase, string consoleInput, int fixedWheelAmount, int expected)
         {
             // Redirect the console input to a string, ';' is used to separate line inputs
-            var stringBuilder = new StringBuilder();
-            foreach (var str in consoleInput.Split(';') )
-            {
-                stringBuilder.AppendLine(str.Trim());
-            }
-            var stringReader = new StringReader(stringBuilder.ToString());
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
             Console.SetIn(stringReader);
 
             // setup the mock ICategorizedPhrases so the phrase is our test value
@@ -49,6 +44,116 @@ namespace UnitTests
             //int expected = fixedWheelAmount*lettersGuessedCorrectly + PhraseBoard.PointsEarnedForSolving;
             int actual = game.players[0].TurnScore;
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("pulp fiction", "Movies", "1; Diane; 2;pulp fiction;")]
+        public void TestMain(string arg0, string arg1, string consoleInput)
+        {
+            // Redirect the console input to a string, ';' is used to separate line inputs
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
+            Console.SetIn(stringReader);
+
+            //Act
+            Program.Main(new string[] { arg0, arg1 });
+
+            // Assert
+
+        }
+        [Theory]
+        [InlineData("1;Diane", new string[] { "Diane" })]
+        [InlineData("a;2;;Wolf", new string[] { "Player 1", "Wolf" })]
+        public void UIGetPlayerNamesTests(string consoleInput, string[] expected)
+        {
+
+            // Arrange
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
+            Console.SetIn(stringReader);
+            var sut = new GameUI();
+
+            // Act
+            var actual = sut.GetPlayerNames().ToArray();
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("a;2", 2)]
+        public void UIGetUserChoice_Tests(string consoleInput, int expected)
+        {
+
+            // Arrange
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
+            Console.SetIn(stringReader);
+            var sut = new GameUI();
+
+            // Act
+            var actual = sut.GetUserChoice(); ;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("c", "abc", new char[] { }, 'c')]
+        [InlineData("a;c", "abc", new char[] { }, 'c')]
+        [InlineData("a;c", "abc", new char[] { 'a' }, 'c')]
+        [InlineData("a", "abc", new char[] { 'c' }, 'a')]
+        public void UIGetSpinGuessLetter_Tests(string consoleInput, string phraseString, char[] guesses, char expected)
+        {
+
+            // Arrange
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
+            Console.SetIn(stringReader);
+            var sut = new GameUI();
+            var phraseBoard = new PhraseBoard(phraseString);
+            var player = new Player("Player");
+            foreach (char guess in guesses)
+            {
+                var points = phraseBoard.MakeGuess(1000, guess);
+                player.AddCurrentScore(points);
+            }
+
+            // Act
+            var actual = sut.GetSpinGuessLetter(phraseBoard, player);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("ABC", "abc")]
+        [InlineData("Hello World", "hello world")]
+        public void UIGetSolveGuess_Tests(string consoleInput, string expected)
+        {
+
+            // Arrange
+            var stringReader = new StringReader(Util.FormatConsoleInput(consoleInput));
+            Console.SetIn(stringReader);
+            var sut = new GameUI();
+
+            // Act
+            var actual = sut.GetSolveGuess(); ;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+
+    public class Util
+    { 
+        internal static string FormatConsoleInput(string consoleInput)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var str in consoleInput.Split(';'))
+            {
+                stringBuilder.AppendLine(str.Trim());
+            }
+            return stringBuilder.ToString();
         }
 
     }
